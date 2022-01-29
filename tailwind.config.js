e37f97ed1992770ec56c5c96044fce7b45ee16cb
Meer Ahmed <you@example.com>
2022-01-28 20:39:23 -0500

    LandingPage

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  DarkMode: 'Media',
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'bgimg': 'url("./Images/BGimg.jpg")',
+      }
+    },
+  },
+  plugins: [],
+}
