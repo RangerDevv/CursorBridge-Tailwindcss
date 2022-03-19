6797c3e1b87fdbc76ea0fd48afa8fd84414cb322
Rangerdevv <meerabdul2008@gmail.com>
2022-03-18 22:40:26 -0400

    deleted images

--- Components/icons/controller.js
+++ Components/icons/controller.js
@@ -8,14 +8,10 @@ const SVGcontent = document.getElementById("SVGcontent");
 const IMGcontent = document.getElementById("IMGcontent");
 
 SVG.onclick = function(){
-    HTMLcontent.style.display = "none";
-    CSScontent.style.display = "none";
     SVGcontent.style.display = "block";
     IMGcontent.style.display = "none";
 }
 IMG.onclick = function(){
-    HTMLcontent.style.display = "none";
-    CSScontent.style.display = "none";
     SVGcontent.style.display = "none";
     IMGcontent.style.display = "block";
 }
